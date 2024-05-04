18a3add521050fad42a58ea8610f5fa8027b077e
Marc Cueto <gimpage@hotmail.com>
2024-05-03 22:31:38 -0400

    chore: Remove @bdhamithkumara/react-push-notification package and unused code

--- pomodoro.ui/src/components/timer.tsx
+++ pomodoro.ui/src/components/timer.tsx
@@ -1,16 +1,15 @@
-import addNotification from "@bdhamithkumara/react-push-notification";
-
 const Timer: React.FC = () => {
   function sendNotification() {
-    setTimeout(() => {
-      addNotification({
-        title: "Pomodoro Timer",
-        // subtitle: "This is a subtitle",
-        message: "Time to take a break!",
-        theme: "red",
-        native: true, // when using native, your OS will handle theming.
-      });
-    }, 2000);
+    // setTimeout(() => {
+    //   addNotification({
+    //     title: "Pomodoro Timer",
+    //     // subtitle: "This is a subtitle",
+    //     message: "Time to take a break!",
+    //     theme: "red",
+    //     native: true, // when using native, your OS will handle theming.
+    //   });
+    // }, 2000);
+    alert("Notification sent!");
   }
   return (
     <div>
